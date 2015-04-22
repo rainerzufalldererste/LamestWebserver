9a299e092ea9af4c5b72e878602f8dcc7bbdace1
Christoph Stiller <c.stiller@live.de>
2015-04-22 14:59:56 +0200

    megaserver

--- LamestWebserver/LamestWebserver/LServer.cs
+++ LamestWebserver/LamestWebserver/LServer.cs
@@ -13,13 +13,54 @@ namespace LamestWebserver
     {
         TcpListener tcpList;
         List<Thread> threads = new List<Thread>();
+        Thread mThread;
+        public int port;
+        public string folder = "./web";
+        public bool running = true;
 
         public LServer(int port)
         {
             this.tcpList = new TcpListener(IPAddress.Any, port);
-            Thread t = new Thread(new ThreadStart(ListenAndStuff));
-            t.Start();
+            mThread = new Thread(new ThreadStart(ListenAndStuff));
+            mThread.Start();
+            this.port = port;
         }
+
+        public void killMe()
+        {
+
+            running = false;
+
+            try
+            {
+                tcpList.Stop();
+            }
+            catch (Exception e) { Console.WriteLine(port + ": " + e.Message); }
+
+            try
+            {
+                mThread.Abort();
+            }
+            catch (Exception e) { Console.WriteLine(port + ": " + e.Message); }
+
+            Console.WriteLine("Main Thread Dead! - port: " + port + " - folder: " + folder);
+
+            int i = threads.Count;
+
+            while (threads.Count > 0)
+            {
+                try
+                {
+                    threads[0].Abort();
+                }
+                catch (Exception e) { Console.WriteLine(port + ": " + e.Message); }
+                threads.RemoveAt(0);
+
+                Console.WriteLine("Thread Dead! (" + threads.Count + "/" + i + ") - port: " + port + " - folder: " + folder);
+            }
+        }
+
+        public int getThreadCount() { return threads.Count + 1; }
 
         private void ListenAndStuff()
         {
@@ -31,12 +72,16 @@ namespace LamestWebserver
             catch (Exception e) { Console.WriteLine("I Hate Servers! OTHA PORTZ! " + e.Message); return; };
 
 
-            while (true)
+            while (running)
             {
-                TcpClient tcpClient = this.tcpList.AcceptTcpClient();
-                threads.Add(new Thread(new ParameterizedThreadStart(DoStuff)));
-                threads[threads.Count - 1].Start((object)tcpClient);
-                Console.WriteLine("Client Connected!...");
+                try
+                {
+                    TcpClient tcpClient = this.tcpList.AcceptTcpClient();
+                    threads.Add(new Thread(new ParameterizedThreadStart(DoStuff)));
+                    threads[threads.Count - 1].Start((object)tcpClient);
+                    Console.WriteLine("Client Connected!...");
+                }
+                catch (Exception e) { Console.WriteLine("Something failed... Yes. " + e.Message); };
             }
         }
 
@@ -49,19 +94,19 @@ namespace LamestWebserver
             byte[] msg = new byte[4096];
             int bytes;
 
-            while(true)
+            while (running)
             {
                 try
                 {
                     bytes = nws.Read(msg, 0, 4096);
                 }
-                catch(Exception e)
+                catch (Exception e)
                 {
                     Console.WriteLine("An error occured! " + e.Message);
                     break;
                 }
 
-                if(bytes == 0)
+                if (bytes == 0)
                 {
                     Console.WriteLine("An error occured! ");
                     //break;
@@ -92,24 +137,52 @@ namespace LamestWebserver
                     {
                         */
 
+
+
                     try
                     {
-                        if (System.IO.File.Exists("./web" + htp.data))
-                        {
-                            string s = System.IO.File.ReadAllText("./web" + htp.data);
-                            HTTP_Packet htp_ = new HTTP_Packet() { version = "HTTP/1.1", status = "200 OK", data = s, contentLength = enc.GetBytes(s).Length };
-                            buffer = enc.GetBytes(htp_.getPackage());
-                            ns.Write(buffer, 0, buffer.Length);
 
-                            Console.WriteLine(htp_.getPackage());
+                        if (htp.data[htp.data.Length - 1] == '\\' || htp.data[htp.data.Length - 1] == '/')
+                        {
+                            if (System.IO.File.Exists(folder + htp.data + "index.html"))
+                            {
+                                string s = System.IO.File.ReadAllText(folder + htp.data + "index.html");
+                                HTTP_Packet htp_ = new HTTP_Packet() { version = "HTTP/1.1", status = "200 OK", data = s, contentLength = enc.GetBytes(s).Length };
+                                buffer = enc.GetBytes(htp_.getPackage());
+                                ns.Write(buffer, 0, buffer.Length);
+
+                                Console.WriteLine(htp_.getPackage());
+                            }
+                            else
+                            {
+                                HTTP_Packet htp_ = new HTTP_Packet() { version = "HTTP/1.1", status = "403 Forbidden", data = "<title>Error 403: Forbidden</title><body style='background-color: #f0f0f2;'><div style='font-family: sans-serif;width: 600px;margin: 5em auto;padding: 50px;background-color: #fff;border-radius: 1em;'><h1>Error 403: Forbidden!</h1><p>Access denied to: " + htp.data + "</p><hr><p>The Package you were sending: <br><br>" + msg_.Replace("\r\n", "<br>") + "</p><hr><br><p>I guess you don't know what that means. You're welcome! I'm done here!</p><p style='text-align:right'>- LamestWebserver (LameOS)</p></div></body>" };
+                                htp_.contentLength = enc.GetBytes(htp_.data).Length;
+                                buffer = enc.GetBytes(htp_.getPackage());
+                                ns.Write(buffer, 0, buffer.Length);
+
+                                Console.WriteLine(htp_.getPackage());
+                            }
                         }
                         else
                         {
-                            HTTP_Packet htp_ = new HTTP_Packet() { version = "HTTP/1.1", status = "404 Not Found", data = "", contentLength = 0 };
-                            buffer = enc.GetBytes(htp_.getPackage());
-                            ns.Write(buffer, 0, buffer.Length);
+                            if (System.IO.File.Exists(folder + htp.data))
+                            {
+                                string s = System.IO.File.ReadAllText(folder + htp.data);
+                                HTTP_Packet htp_ = new HTTP_Packet() { version = "HTTP/1.1", status = "200 OK", data = s, contentLength = enc.GetBytes(s).Length };
+                                buffer = enc.GetBytes(htp_.getPackage());
+                                ns.Write(buffer, 0, buffer.Length);
 
-                            Console.WriteLine(htp_.getPackage());
+                                Console.WriteLine(htp_.getPackage());
+                            }
+                            else
+                            {
+                                HTTP_Packet htp_ = new HTTP_Packet() { version = "HTTP/1.1", status = "404 Not Found", data = "<title>Error 404: Page Not Found</title><body style='background-color: #f0f0f2;'><div style='font-family: sans-serif;width: 600px;margin: 5em auto;padding: 50px;background-color: #fff;border-radius: 1em;'><h1>Error 404: Page Not Found!</h1><p>The following file could not be found: " + htp.data + "</p><hr><p>The Package you were sending: <br><br>" + msg_.Replace("\r\n", "<br>") + "</p><hr><br><p>I guess you don't know what that means. You're welcome! I'm done here!</p><p style='text-align:right'>- LamestWebserver (LameOS)</p></div></body>" };
+                                htp_.contentLength = enc.GetBytes(htp_.data).Length;
+                                buffer = enc.GetBytes(htp_.getPackage());
+                                ns.Write(buffer, 0, buffer.Length);
+
+                                Console.WriteLine(htp_.getPackage());
+                            }
                         }
                     }
                     catch (Exception e)
@@ -118,7 +191,7 @@ namespace LamestWebserver
                     }
                     //}
                 }
-                catch(Exception e)
+                catch (Exception e)
                 {
                     Console.WriteLine("nope. " + e.Message);
                 }
--- LamestWebserver/LamestWebserver/Program.cs
+++ LamestWebserver/LamestWebserver/Program.cs
@@ -10,6 +10,7 @@ namespace LamestWebserver
     {
         static void Main(string[] args)
         {
+            List<LServer> ports = new List<LServer>();
 
             Console.WriteLine("Happy Server! Today is " + DateTime.Now.ToString() + " if you are wondering why you are using such a strange server...");
             Console.WriteLine();
@@ -21,26 +22,32 @@ namespace LamestWebserver
                 {
 
                     Console.WriteLine("Please specify ports in param... trying to use 8080... i will fail - or not - yes - true!  - example: \"lws 8080\"");
-                    LServer lserver = new LServer(8080);
+                    ports.Add(new LServer(8080));
                 }
                 catch (Exception e) { Console.WriteLine("I Hate Servers! " + e.Message); };
             }
             else if(args.Length == 1)
             {
-                try
+                if (args[0] == "-clean")
                 {
-                    LServer lserver = new LServer(Int32.Parse(args[0])); //does stuff
+                    Console.WriteLine("Clean Server Started.");
                 }
-                catch (Exception e) { Console.WriteLine("I Hate Servers! " + e.Message); };
-            }
-            else
-            {
-                int i = 8080;
-                while (true)
+                else
                 {
                     try
                     {
-                        LServer lserver = new LServer(i++); //does stuff
+                        ports.Add(new LServer(Int32.Parse(args[0]))); //does stuff
+                    }
+                    catch (Exception e) { Console.WriteLine("I Hate Servers! " + e.Message); };
+                }
+            }
+            else
+            {
+                for(int i = 0; i < args.Length; i++)
+                {
+                    try
+                    {
+                        ports.Add(new LServer(Int32.Parse(args[i]))); //does stuff
                     }
                     catch (Exception e) { Console.WriteLine("I Hate Servers! " + e.Message); };
                 }
@@ -64,6 +71,51 @@ namespace LamestWebserver
                     {
                         break;
                     }
+                }
+
+                switch(s)
+                {
+                    case "ports": { for (int i = 0; i < ports.Count; i++) { Console.WriteLine("Port: " + ports[i].port + " Folder: " + ports[i].folder + " Threads: " + ports[i].getThreadCount()); } };
+                        Console.WriteLine("Done!");
+                        break;
+
+                    case "kill": {
+                        try
+                        {
+                            Console.WriteLine("Port: ");
+                            string id = Console.ReadLine();
+                            for (int i = 0; i < ports.Count; i++)
+                            {
+                                if (ports[i].port == Int32.Parse(id))
+                                {
+                                    ports[i].killMe();
+                                    ports.RemoveAt(i);
+                                    Console.WriteLine("Done!");
+                                    break;
+                                }
+                            }
+                        }
+                        catch (Exception e) { Console.WriteLine("Failed!"); }
+                    };
+                        break;
+
+                    case "new":
+                        {
+                            try
+                            {
+                                Console.WriteLine("Port:");
+                                string prt = Console.ReadLine();
+                                Console.WriteLine("Folder: (\".\\web\")");
+                                string fld = Console.ReadLine();
+                                ports.Add(new LServer(Int32.Parse(prt)) { folder = fld });
+                                Console.WriteLine("Done!");
+                            }
+                            catch (Exception e) { Console.WriteLine("Failed!"); }
+                        };
+                        break;
+
+                    default: { Console.WriteLine("Hello " + s + "!"); }
+                        break;
                 }
             }
         }
