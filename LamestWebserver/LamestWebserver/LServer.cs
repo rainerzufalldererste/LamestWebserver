d5a05445c1d79a3bca1eb16dafdfe10e6ddd7918
Christoph Stiller <c.stiller@live.de>
2015-04-27 09:12:36 +0200

    thready party

--- LamestWebserver/LamestWebserver/LServer.cs
+++ LamestWebserver/LamestWebserver/LServer.cs
@@ -87,7 +87,9 @@ namespace LamestWebserver
 
                 while (i < threads.Count)
                 {
-                    if (threads[i].ThreadState == ThreadState.Running || threads[i].ThreadState == ThreadState.Unstarted)
+                    if (threads[i].ThreadState == ThreadState.Running ||
+                        threads[i].ThreadState == ThreadState.Unstarted ||
+                        threads[i].ThreadState == ThreadState.AbortRequested)
                     {
                         i++;
                     }
