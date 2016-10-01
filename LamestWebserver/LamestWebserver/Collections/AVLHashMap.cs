9ca0caadb2f23a1f77b893b8c0efc0577cf5163d
Christoph Stiller <c.stiller@live.de>
2016-10-01 15:46:46 +0200

    starting on more performant collections

---
+++ LamestWebserver/LamestWebserver/Collections/AVLHashMap.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace LamestWebserver.Collections
+{
+    public class AVLHashMap<TKey, TValue> : IDictionary<TKey, TValue>, ISerializable where TKey : IEqualityComparer
+    {
+        private int size = 1024;
+        protected int elementCount = 0;
+
+        private object[] HashMap;
+        
+        private FriendlyQueue<TKey, TValue> lastAdded = null;
+
+        public AVLHashMap(int size = 1024, uint? maxSize = null)
+        {
+            this.size = size;
+            if (maxSize.HasValue && maxSize > 0)
+            {
+                HashMap = new object[size];
+                lastAdded = new FriendlyQueue<TKey, TValue>(maxSize, (KeyValuePair<TKey, TValue> item) => { Remove(item); });
+            }
+        }
+
+        public ICollection<TKey> Keys
+        {
+            get
+            {
+                throw new InvalidOperationException("This Dictinary does not support getting all Keys");
+            }
+        }
+
+        public ICollection<TValue> Values
+        {
+            get
+            {
+                throw new InvalidOperationException("This Dictinary does not support getting all Values");
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return elementCount;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                int hash = Math.Abs(key.GetHashCode()) % size;
+
+                if (HashMap[hash] == null)
+                {
+                    return default(TValue); // Chris: Should we throw an exception instead?
+                }
+                else if (HashMap[hash] is KeyValuePair<TKey, TValue>)
+                {
+                    if (((KeyValuePair<TKey, TValue>)HashMap[hash]).Key.Equals(key))
+                        return ((KeyValuePair<TKey, TValue>)HashMap[hash]).Value;
+                    else
+                        return default(TValue);
+                }
+                else // if HashMap[hash] is an AVL Node search for it
+                    throw new NotImplementedException();
+            }
+
+            set
+            {
+                Add(new KeyValuePair<TKey, TValue>(key, value));
+            }
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            try
+            {
+                if (this[key].Equals(default(TValue)))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            Add(new KeyValuePair<TKey, TValue>(key, value));
+        }
+
+        public bool Remove(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            try
+            {
+                value = this[key];
+                return true;
+            }
+            catch(Exception)
+            {
+                value = default(TValue);
+                return false;
+            }
+        }
+
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            int hash = Math.Abs(item.Key.GetHashCode()) % size;
+
+            if (HashMap[hash] == null)
+            {
+                HashMap[hash] = item;
+                elementCount++;
+            }
+            else if(HashMap[hash] is KeyValuePair<TKey, TValue>)
+            {
+                if(((KeyValuePair<TKey, TValue>)HashMap[hash]).Key.Equals(item.Key))
+                {
+                    lastAdded.Remove(item.Key);
+                    HashMap[hash] = item;
+                    lastAdded.Enqueue(item);
+                }
+                // TODO: Create AVL Tree and add; if exists don't add to elementCount and Dequeue old item if maxSize.HasValue; if !exists add to elementCount
+            }
+            else
+            {
+                // TODO: Add to AVLTree; if exists don't add to elementCount and Dequeue old item if maxSize.HasValue; if !exists add to elementCount
+            }
+
+            if (lastAdded != null)
+            {
+                lastAdded.Enqueue(item);
+            }
+        }
+
+        public void Clear()
+        {
+            elementCount = 0;
+            HashMap = new object[size];
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            try
+            {
+                TValue element = this[item.Key];
+
+                if (element.Equals(item.Value))
+                    return true;
+                else
+                    return false;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue(nameof(size), size);
+            info.AddValue(nameof(elementCount), elementCount);
+            info.AddValue(nameof(HashMap), HashMap);
+        }
+
+        public AVLHashMap(SerializationInfo info, StreamingContext context)
+        {
+            size = info.GetInt32(nameof(size));
+            elementCount = info.GetInt32(nameof(elementCount));
+            HashMap = (object[])info.GetValue(nameof(HashMap), typeof(object[]));
+        }
+    }
+}
---
+++ LamestWebserver/LamestWebserver/Collections/FriendlyQueue.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LamestWebserver.Collections
+{
+    public class FriendlyQueue<TKey, TValue> : ISerializable where TKey : IEqualityComparer
+    {
+        private QueueElement first, last;
+        public uint size { get; private set; }
+        public uint? maxSize = null;
+        public event Action<KeyValuePair<TKey, TValue>> callbackOnDelete;
+
+        public FriendlyQueue() { }
+
+        public FriendlyQueue(uint? maxSize = null, Action<KeyValuePair<TKey, TValue>> callback = null)
+        {
+            this.maxSize = maxSize;
+
+            if (callback != null)
+                callbackOnDelete += callback;
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue(nameof(first), first);
+            info.AddValue(nameof(last), last);
+            info.AddValue(nameof(size), size);
+            info.AddValue(nameof(maxSize), maxSize);
+        }
+
+        public void Remove(TKey key)
+        {
+            if (first.data.Key.Equals(key))
+            {
+                first = first.next;
+            }
+            else if (last.data.Key.Equals(key))
+            {
+                QueueElement.Remove(key, first);
+                size--; // Chris: IT HAS ALREADY BEEN FOUND THROUGH SPECIFICATION IN THE IF STATEMENT
+                last = QueueElement.FindLast(first);
+            }
+            else
+                if (QueueElement.Remove(key, first))
+                    size--;
+        }
+
+        public KeyValuePair<TKey, TValue> Find(TKey key)
+        {
+            return QueueElement.Find(key, first).data;
+        }
+
+        public void Enqueue(KeyValuePair<TKey, TValue> item)
+        {
+            if (first == null)
+            {
+                var tmp = new QueueElement(item);
+                first = tmp;
+                last = tmp;
+                size++;
+
+                System.Diagnostics.Debug.Assert(size == 1);
+            }
+            else
+            {
+                new QueueElement(item, last);
+                size++;
+            }
+
+            if(maxSize.HasValue)
+            {
+                if(size > maxSize)
+                {
+                    callbackOnDelete(Dequeue());
+                }
+            }
+        }
+
+        public KeyValuePair<TKey, TValue> Dequeue()
+        {
+            var ret = first.data;
+            first = first.next;
+            return ret;
+        }
+
+        public KeyValuePair<TKey, TValue> Peek()
+        {
+            return first.data;
+        }
+
+        public class QueueElement : ISerializable
+        {
+            internal QueueElement next = null;
+            internal KeyValuePair<TKey, TValue> data;
+
+            public QueueElement()
+            {
+
+            }
+
+            public QueueElement(KeyValuePair<TKey, TValue> item)
+            {
+                this.data = item;
+            }
+
+            public QueueElement(KeyValuePair<TKey, TValue> item, QueueElement father)
+            {
+                this.data = item;
+
+                if(father.next != null)
+                {
+                    var old = father.next;
+                    this.next = old;
+                    father.next = this;
+                }
+                else
+                    father.next = this;
+            }
+
+            public static QueueElement Find(TKey key, QueueElement first)
+            {
+                while(!first.data.Key.Equals(key))
+                {
+                    if (first.next == null)
+                        return null;
+
+                    first = first.next;
+                }
+
+                return first;
+            }
+
+            public static bool Remove(TKey key, QueueElement first)
+            {
+                while (first.next != null && !first.next.data.Key.Equals(key))
+                {
+                    first = first.next;
+                }
+
+                if (first.next != null && first.next.data.Key.Equals(key))
+                {
+                    first.next = first.next.next;
+                    return true;
+                }
+
+                return false;
+            }
+
+            public void GetObjectData(SerializationInfo info, StreamingContext context)
+            {
+                info.AddValue(nameof(next), next);
+                info.AddValue(nameof(data), data);
+            }
+
+            public static QueueElement FindLast(QueueElement first)
+            {
+                while (first.next != null)
+                {
+                    first = first.next;
+                }
+
+                return first;
+            }
+        }
+    }
+}
